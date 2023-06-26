1705d31cc9c35a0f71aa58dd114492ed76bc3946
summer2023acct <137746336+summer2023acct@users.noreply.github.com>
2023-06-25 23:55:08 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Essentially copied the code  from the examples already given from "getmean" in the assignment with a couple variations to make the specific code work
+makeCacheMatrix <- function(x = matrix(s(1:200, 5),4, 4) {
+  a <- NULL
+        set <- function(b)
+        x <<- b
+        a <<- NULL
+get <- function() x
+        setsolve <- function(solve) a <<- solve
+        getsolve <- function() a
+list(set = set, get = get, getsolve = getsolve, setsolve = setsolve)
 }
 
 
 ## Write a short comment describing this function
+## Again just reuse the ideas already given from the "mean" functions already shown
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ a < x$getsolve()
+        if(!isnull(a))
+                message("Matrix is inversing now, be patient.")
+        return(a)
+        }
+data <- x$get()
+a <- solve(example, ...)
+        x$setsolve(a)
+                            
 }
